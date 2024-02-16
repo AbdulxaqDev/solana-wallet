f5ca401f78866b004a336ba846cc8b2c7ee88615
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-17 02:00:08 +0300

    chore: update script

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,3 +1,59 @@
-import { logger } from "./helper";
+import fs from "node:fs/promises";
+import {
+  Connection,
+  Keypair,
+  SystemProgram,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import {
+  startAndConnect,
+  getAccounts,
+  Cleanup,
+} from "solana-test-validator-js";
 
-logger("Hello, world!");
+import __dirname from "./utils/__dirname";
+import { tWallet } from "./types/wallet";
+
+const WALLET_BALANCE = __dirname(import.meta.url, "balance.json");
+
+async function createWallet(): Promise<tWallet[]> {
+  const walletFileHandler = await fs.open(WALLET_BALANCE);
+  const walletData = await walletFileHandler.read();
+
+  try {
+    const wallet = JSON.parse(walletData.toString());
+
+    if (wallet?.length === 0) throw new Error("No wallet, now creating!");
+
+    return wallet;
+  } catch (error) {
+    console.log(error.message);
+
+    const keypair = Keypair.generate();
+    const newWallet = {
+      publicKey: keypair.publicKey.toBase58(),
+      privateKey: keypair.secretKey,
+    };
+    const wallet = [];
+
+    wallet.push(newWallet);
+
+    await walletFileHandler.writeFile(
+      WALLET_BALANCE,
+      JSON.stringify(wallet),
+      ""
+    );
+
+    walletFileHandler.writeFile(WALLET_BALANCE, "Hey there!", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+
+    return wallet;
+  }
+}
+console.log("----");
+
+console.log(await createWallet());
