b22db475f59d2948a30ada7f15a1ac01dffd8060
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-19 17:51:34 +0300

    feat: add balance getter and logger helpers

--- src/helpers/wallet.helpers.ts
+++ src/helpers/wallet.helpers.ts
@@ -1,4 +1,4 @@
-import { Keypair } from "@solana/web3.js";
+import { Keypair, Connection, PublicKey } from "@solana/web3.js";
 import { tWallet } from "src/types/wallet.types";
 
 export function generateKeypairs(): tWallet {
@@ -6,5 +6,29 @@ export function generateKeypairs(): tWallet {
   return {
     publicKey: keypair.publicKey,
     privateKey: keypair.secretKey,
+  };
+}
+
+export function balanceLogger(sender: number, recipient: number) {
+  console.log(
+    "  \x1b[36m%s\x1b[0m",
+    `Sender wallet balance: ${sender} Lamport`
+  );
+  console.log(
+    "  \x1b[36m%s\x1b[0m",
+    `Recipient wallet balance: ${recipient} Lamport`
+  );
+}
+
+export async function getWalletsBalance(
+  connection: Connection,
+  senderPubKey: PublicKey,
+  recPubKey: PublicKey
+): Promise<{ senderBalance: number; recBalance: number }> {
+  senderPubKey = new PublicKey(senderPubKey);
+  recPubKey = new PublicKey(recPubKey);
+  return {
+    senderBalance: await connection.getBalance(senderPubKey),
+    recBalance: await connection.getBalance(recPubKey),
   };
 }
